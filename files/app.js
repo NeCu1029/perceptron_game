6079551fbcf6661a1cc949ff32adf2cb135a8c17
NeCu1029 <necu1029@gmail.com>
2025-10-25 20:44:32 +0900

    change game details

--- files/app.js
+++ files/app.js
@@ -69,7 +69,7 @@ function drawGame() {
     circle(WIDTH * show2[i][0], HEIGHT * show2[i][1], WIDTH * 0.01);
   }
 
-  fillAndLineSet(255, 0, 0, 255, 0, 0, 3);
+  fillAndLineSet(0, 0, 0, 0, 0, 0, 3);
   circle(centX, centY, WIDTH * 0.02);
   line(
     centX + 3 * WIDTH * Math.cos(rot),
@@ -158,7 +158,7 @@ function set1() {
   background(230);
   textSet(0, 0, 0, WIDTH * 0.025);
   text(
-    "<규칙>\n\n검은 점과 흰 점이 주어집니다. (전체 1000개 중 랜덤 200개)\n빨간색 선을 움직여 화살표가 있는 쪽에만 흰 점이 위치하도록 합니다.\n화살표는 선 위에 있는 중심점에 달려 있으며, 항상 선에 수직합니다.\nA/D키로 중심점을 이동할 수 있습니다. (D키가 화살표 쪽)\nW/S키로 선을 회전할 수 있습니다. (W키가 시계 방향)\n선이 이동할 때 미끄러짐이 작용합니다. \n15초 후 올바르게 분류한 점의 개수가 점수가 됩니다. (시작할 때 보여주지 않은 점 포함)\n\n스페이스 바 눌러 시작",
+    "<규칙>\n\n파란색 점과 주황색 점이 주어집니다. (전체 1000개 중 랜덤 20개)\n검은색 선을 움직여 화살표가 있는 쪽에만 주황색 점이 위치하도록 합니다.\n화살표는 선 위에 있는 중심점에 달려 있으며, 항상 선에 수직합니다.\nW/S키로 화살표를 따라 중심점을 이동할 수 있습니다. (W키가 화살표 쪽)\nA/D키로 중심점을 기준으로 선을 회전할 수 있습니다. (D키가 시계 방향)\n선이 이동할 때 미끄러짐이 작용합니다.\n30초 후 올바르게 분류한 점의 개수가 점수가 됩니다. (시작할 때 보여주지 않은 점 포함)\n\n스페이스 바를 눌러 시작",
     WIDTH * 0.5,
     HEIGHT * 0.5
   );
@@ -196,6 +196,7 @@ function keep2() {
 }
 
 function set3() {
+  // 3번 장면 시작
   for (let i = 0; i < 10; i++) {
     tmp = i * 50 + Math.floor(50 * Math.random());
     show1[i][0] = pts1[tmp][0];
@@ -210,18 +211,26 @@ function set3() {
   rot = Math.random() * 2 * Math.PI;
   lvel = 0;
   avel = 0;
+  timeTrack1 = new Date().getTime();
 
   drawGame();
 }
 
 function keep3() {
+  // 3번 장면 지속
+  timeTrack2 = new Date().getTime();
+  if (timeTrack2 - timeTrack1 >= 30000) {
+    stat = 4;
+    change = true;
+  }
+
   lvel *= 0.99;
   avel *= 0.95;
 
-  if (key === "a" && keyIsPressed) lvel += WIDTH / 3600;
-  else if (key === "d" && keyIsPressed) lvel -= WIDTH / 3600;
-  else if (key === "w" && keyIsPressed) avel += Math.PI / 450;
-  else if (key === "s" && keyIsPressed) avel -= Math.PI / 450;
+  if (key === "s" && keyIsPressed) lvel += WIDTH / 3600;
+  else if (key === "w" && keyIsPressed) lvel -= WIDTH / 3600;
+  else if (key === "d" && keyIsPressed) avel += Math.PI / 450;
+  else if (key === "a" && keyIsPressed) avel -= Math.PI / 450;
   lvel = Math.min(Math.max(lvel, -WIDTH / 120), WIDTH / 120);
   avel = Math.min(Math.max(avel, -Math.PI / 15), Math.PI / 15);
 
@@ -236,6 +245,70 @@ function keep3() {
   drawGame();
 }
 
+function set4() {
+  // 4번 장면 시작
+  background(230);
+  fillAndLineSet(0, 150, 255, 0, 0, 0, 1);
+  for (let i = 0; i < 500; i++) {
+    circle(WIDTH * pts1[i][0], HEIGHT * pts1[i][1], WIDTH * 0.01);
+  }
+  fillAndLineSet(255, 100, 0, 0, 0, 0, 1);
+  for (let i = 0; i < 500; i++) {
+    circle(WIDTH * pts2[i][0], HEIGHT * pts2[i][1], WIDTH * 0.01);
+  }
+
+  fillAndLineSet(0, 0, 0, 0, 0, 0, 3);
+  circle(centX, centY, WIDTH * 0.02);
+  line(
+    centX + 3 * WIDTH * Math.cos(rot),
+    centY + 3 * WIDTH * Math.sin(rot),
+    centX - 3 * WIDTH * Math.cos(rot),
+    centY - 3 * WIDTH * Math.sin(rot)
+  );
+  line(
+    centX,
+    centY,
+    centX - 0.05 * WIDTH * Math.sin(rot),
+    centY + 0.05 * WIDTH * Math.cos(rot)
+  );
+  line(
+    centX - 0.05 * WIDTH * Math.sin(rot),
+    centY + 0.05 * WIDTH * Math.cos(rot),
+    centX -
+      0.05 * WIDTH * Math.sin(rot) +
+      0.02 * WIDTH * Math.cos(rot + (Math.PI * 4) / 3),
+    centY +
+      0.05 * WIDTH * Math.cos(rot) +
+      0.02 * WIDTH * Math.sin(rot + (Math.PI * 4) / 3)
+  );
+  line(
+    centX - 0.05 * WIDTH * Math.sin(rot),
+    centY + 0.05 * WIDTH * Math.cos(rot),
+    centX -
+      0.05 * WIDTH * Math.sin(rot) +
+      0.02 * WIDTH * Math.cos(rot + (Math.PI * 5) / 3),
+    centY +
+      0.05 * WIDTH * Math.cos(rot) +
+      0.02 * WIDTH * Math.sin(rot + (Math.PI * 5) / 3)
+  );
+
+  timeTrack1 = new Date().getTime();
+}
+
+function keep4() {
+  // 4번 장면 지속
+  timeTrack2 = new Date().getTime();
+  if (timeTrack2 - timeTrack1 >= 2000) {
+    stat = 5;
+    change = true;
+  }
+}
+
+function set5() {
+  // 5번 장면 시작
+  background(230);
+}
+
 function setup() {
   createCanvas(WIDTH, HEIGHT);
   rectMode(CORNERS);
@@ -248,11 +321,14 @@ function draw() {
     else if (stat == 1) set1();
     else if (stat == 2) set2();
     else if (stat == 3) set3();
+    else if (stat == 4) set4();
+    else if (stat == 5) set5();
     change = false;
   } else {
     if (stat == 0) keep0();
     else if (stat == 1) keep1();
     else if (stat == 2) keep2();
     else if (stat == 3) keep3();
+    else if (stat == 4) keep4();
   }
 }
